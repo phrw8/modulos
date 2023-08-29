c94160621688a2a14c1eafd43c4123dd52986a75
Paulo Henrique <ifcphrw@gmail.com>
2023-08-29 15:43:36 -0300

    calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,5 +1,9 @@
 var somar = require("./modulos/somar");
 var media = require("./modulos/media");
+var multiplicar = require("./modulos/multiplicar");
+var dividir = require("./modulos/dividir");
 
 console.log(somar(20,90));
 console.log(media(20,90));
+console.log(multiplicar(20,90));
+console.log(dividir(20,90));
