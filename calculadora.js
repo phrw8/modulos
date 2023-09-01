9c20ef525d4fcdb0dd459fe2f70892d278b74912
Paulo Henrique <ifcphrw@gmail.com>
2023-09-01 08:57:06 -0300

    MENSAGEM DO VASCAO

--- calculadora.js
+++ calculadora.js
@@ -2,8 +2,13 @@ var somar = require("./modulos/somar");
 var media = require("./modulos/media");
 var multiplicar = require("./modulos/multiplicar");
 var dividir = require("./modulos/dividir");
+var mensagem = require("./modulos/mensagem");
+var nome = "Paulo"
 
 console.log(somar(20,90));
 console.log(media(20,90));
 console.log(multiplicar(20,90));
 console.log(dividir(20,90));
+console.log(mensagem (nome))
+
+//paulada//
---
+++ modulos/mensagem.js
@@ -0,0 +1,4 @@
+var mensagem = function (nome) {
+    return "ola " + nome + " o gigante da colina despertou";
+  }; 
+  module.exports = mensagem;
